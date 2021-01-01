28edc82c0448659a73a19d50a282d8b10c757e6f
Miguel Ricaño <mricanho@gmail.com>
2021-01-01 11:16:57 -0600

    Add tutorial 3

--- main.sql
+++ main.sql
@@ -118,3 +118,46 @@ SELECT name
   AND name LIKE '%i%'
   AND name LIKE '%o%'
   AND name LIKE '%u%';
+
+-- Tutorial 3
+
+SELECT yr, subject, winner
+  FROM nobel
+  WHERE yr = 1950;
+
+SELECT winner
+  FROM nobel
+  WHERE yr = 1962
+  AND subject = 'Literature';
+
+SELECT yr, subject
+  FROM nobel
+  WHERE winner = 'Albert Einstein';
+
+SELECT winner
+  FROM nobel
+  WHERE subject = 'Peace'
+  AND yr >= 2000;
+
+SELECT yr, subject, winner
+  FROM nobel
+  WHERE subject = 'Literature'
+  AND yr BETWEEN 1980 AND 1989;
+
+SELECT * FROM nobel
+  WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter', 'Barack Obama');
+
+SELECT winner FROM nobel
+  WHERE winner like 'John %';
+
+SELECT yr, subject, winner FROM nobel
+  WHERE (yr = 1980 AND subject = 'Physics')
+  OR (yr = 1984 AND subject = 'Chemistry');
+
+SELECT yr, subject, winner FROM nobel
+  WHERE yr = 1980
+  AND subject NOT IN ('Chemistry', 'Medicine');
+
+SELECT yr, subject, winner FROM nobel
+  WHERE (subject = 'Medicine' AND yr < 1910)
+  OR (subject = 'Literature' AND yr >= 2004);
