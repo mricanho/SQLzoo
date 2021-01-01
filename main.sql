eadb079161b281a0601fd0c8d31d61794ea6aa03
Miguel Ricaño <mricanho@gmail.com>
2021-01-01 11:49:14 -0600

    Add tutorial 9 window function

--- main.sql
+++ main.sql
@@ -438,3 +438,35 @@ FROM nss x
 WHERE question='Q01'
 AND (institution LIKE '%Manchester%')
 GROUP BY institution;
+
+-- Tutorial 9
+-- Window function
+
+SELECT lastName, party, votes
+FROM ge
+WHERE constituency = 'S14000024' AND yr = 2017
+ORDER BY votes DESC;
+
+SELECT party, votes,
+RANK() OVER (ORDER BY votes DESC) as posn
+FROM ge
+WHERE constituency = 'S14000024' AND yr = 2017
+ORDER BY party;
+
+SELECT yr,party, votes,
+RANK() OVER (PARTITION BY yr ORDER BY votes DESC) as posn
+FROM ge
+WHERE constituency = 'S14000021'
+ORDER BY party,yr;
+
+SELECT constituency, party
+FROM ge x 
+WHERE constituency BETWEEN 'S14000021' AND 'S14000026'
+AND yr  = 2017 AND votes >= ALL(SELECT votes FROM ge y  WHERE x.constituency = y. constituency AND y.yr = 2017)
+ORDER BY constituency,votes DESC;
+
+SELECT party , COUNT(*)
+FROM ge x 
+WHERE constituency like 'S%'
+AND yr  = 2017 AND votes >= ALL(SELECT votes FROM ge y  WHERE x.constituency = y. constituency AND y.yr = 2017)
+GROUP BY party;
